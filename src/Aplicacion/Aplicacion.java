6a07fb59cddb8fddda1ba5e553a261186efe6ad9
Kevin Piazzoli Felix Perez <kevinpiazzolistudio@gmail.com>
2019-10-09 01:44:54 -0500

    Revert "09/10/2019 N4 - Creacion del Proyecto"
    This reverts commit 97e81c2144a9ed1e29da47121d1e5c681d49032f.

---
+++ src/Aplicacion/Aplicacion.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Aplicacion;
+
+/**
+ *
+ * @author user
+ */
+public class Aplicacion {
+    
+}
