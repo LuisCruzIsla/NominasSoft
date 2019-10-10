8504a70352824652839326a97566680115a9911d
LuisCruzIsla <luisfelipe.cruzisla@gmail.com>
2019-10-10 01:43:18 -0500

    10/10/2019 M2 - Reestructuracion de paquetes

--- src/Capa2_Aplicacion/Aplicacion.java
+++ src/Capa2_Aplicacion/Aplicacion.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Aplicacion;
+package Capa2_Aplicacion;
 
 /**
  *
--- src/Capa3_Dominio/Contrato.java
+++ src/Capa3_Dominio/Contrato.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Dominio;
+package Capa3_Dominio;
 
 import java.util.ArrayList;
 import java.util.Date;
